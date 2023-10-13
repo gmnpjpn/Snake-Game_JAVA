31ed59d7ea6410b350c0d088224f124bfbdb1922
German Pajarero <germanpajareropino@gmail.com>
2023-10-13 11:55:46 +0200

    Added frame properties

--- src/App.java
+++ src/App.java
@@ -2,5 +2,16 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        int frameWidth = 600;
+        int frameHeight = 600;
+
+        JFrame frame = new JFrame("Snake");
+        frame.setVisible(true);
+        frame.setSize(frameWidth, frameHeight);
+        frame.setLocationRelativeTo(null); // Open the window in the center of the screen
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
     }
 }
