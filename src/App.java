fd955eaab5e7fc325da323846d9676ef1ed8406a
German Pajarero <germanpajareropino@gmail.com>
2023-10-13 16:13:31 +0200

    snakeGame.java constructor created and initialized in the main method. Frame size adjusted to have a propper visualization

--- src/App.java
+++ src/App.java
@@ -6,14 +6,18 @@ public class App {
         int frameHeight = 600;
         ImageIcon img = new ImageIcon("assets/snakeIcon.png");
 
-        JFrame frame = new JFrame("Snake");
+        JFrame frame = new JFrame("Snake"); // The frame represents ALL the window, including tittle bar.
         frame.setIconImage(img.getImage());
         frame.setVisible(true);
         frame.setSize(frameWidth, frameHeight);
-        frame.setLocationRelativeTo(null); // Open the window in the center of the screen
+        frame.setLocationRelativeTo(null); // Open the window in the center of the screen.
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 
+        snakeGame snakeGame = new snakeGame(frameWidth, frameHeight);
+        frame.add(snakeGame);   // Jpanel inside of the frame
+        frame.pack();   // Adjust the frame size (the tittle bar is inlcuded in the size, and we doesn't want that).
+        
     }
 }
--- src/snakeGame.java
+++ src/snakeGame.java
@@ -4,6 +4,16 @@ import java.awt.event.*;
 import java.util.ArrayList;
 import java.util.Random;
 
-public class snakeGame {
+public class snakeGame extends JPanel{
+    int frameWidth;
+    int frameHeight;
+
+    snakeGame(int frameWidth, int frameHeight){
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
+        setPreferredSize(new Dimension(this.frameWidth, this.frameHeight));
+        setBackground(Color.black);
+        
+    }
     
 }
