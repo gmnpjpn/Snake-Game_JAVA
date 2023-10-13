cdab58c57696145878b36039979c4c5f7e5b6387
German Pajarero <germanpajareropino@gmail.com>
2023-10-13 11:56:53 +0200

    Added an icon to the frame and created a folder to save it (assets/)

--- src/App.java
+++ src/App.java
@@ -4,14 +4,16 @@ public class App {
     public static void main(String[] args) throws Exception {
         int frameWidth = 600;
         int frameHeight = 600;
+        ImageIcon img = new ImageIcon("assets/snakeIcon.png");
 
         JFrame frame = new JFrame("Snake");
+        frame.setIconImage(img.getImage());
         frame.setVisible(true);
         frame.setSize(frameWidth, frameHeight);
         frame.setLocationRelativeTo(null); // Open the window in the center of the screen
         frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        
+
     }
 }
